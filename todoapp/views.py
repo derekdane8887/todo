c31a10b53361c8c0c8fef5c7a41bc98f9f5ad37e
D Sizzle <derekdane8887@hotmail.com>
2017-12-04 01:10:01 -0600

    4rd commit- all working

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
-
 # Create your views here.
 from models import todo
 from django.shortcuts import render_to_response
+from django.shortcuts import render
+from .forms import TodoForm
+ 
+ 
  #Define our function, accept a request
 def todo_list(request): 
  #ORM queries the database for all of the to-do entries.
@@ -14,10 +16,10 @@ def todo_list(request):
 	
 def add_todo(request):
     if request.method == "POST":
-        form = TodoForm(request.Post)
+        form = TodoForm(request.POST)
         if form.is_valid():
             todo_item = form.save(commit=False)
     	    todo_item.save()
     else:
         form = TodoForm()
-    return render(request, 'todo/TodoForm.html', {'form': form})	
+    return render(request, 'TodoForm.html', {'form': form})	
